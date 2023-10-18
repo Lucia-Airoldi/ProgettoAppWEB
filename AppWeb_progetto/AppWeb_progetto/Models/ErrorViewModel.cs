73d9c9958b390062fab9edcd5609f15b80c06004
LUCIA AIROLDI <20029298@studenti.uniupo.it>
2023-10-18 15:34:31 +0200

    Remove vecchio

--- AppWeb_progetto/AppWeb_progetto/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AppWeb_progetto.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
